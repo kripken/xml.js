fddab713cde8d196762a532d11739f37b9e625b1
Steve Terpe <srterpe@gmail.com>
2015-02-04 06:35:01 -0800

    always return {} with `errors` property either null or [].

--- src/wrapper.js
+++ src/wrapper.js
@@ -5,8 +5,12 @@ function validateXML(options) {
 	}
 	;
 
-	/* xmllint.raw.js */
+	/* XMLLINT.RAW.JS */
 
 	Module['return'] = Module['return'].split('\n').slice(0,-2);
-	return Module['return'].length ? Module['return'] : null;
+
+	// Do this is a JSLint style way...
+	return {
+		errors: Module['return'].length ? Module['return'] : null
+	};
 }
