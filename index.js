b9ffb97f957e4d695ed1d3074d3c20d0747f38e8
Oskari Noppa <oskari@arpakuut.io>
2020-07-24 15:26:01 +0300

    Add ESLint and unify styling

--- index.js
+++ index.js
@@ -2,42 +2,42 @@ const { Worker } = require('worker_threads');
 const workerFile = require.resolve('./worker.js');
 
 function validateXML(options) {
-  return new Promise(function validateXMLPromiseCb(resolve, reject) {
-    const worker = new Worker(workerFile, {
-      workerData: options,
-    });
+	return new Promise(function validateXMLPromiseCb(resolve, reject) {
+		const worker = new Worker(workerFile, {
+			workerData: options,
+		});
 
-    let output = ''
+		let output = '';
 
-    function stdout(msg) {
-      output += String.fromCharCode(msg);
-    }
+		function stdout(msg) {
+			output += String.fromCharCode(msg);
+		}
 
-    function onExit(code) {
-      if (code === 0) {
-        resolve({
-          errors: [],
-        })
-      } else if (code === 3 || code === 4 /* validationError */) {
-        resolve({
-          errors: output.length
-            ? output.split('\n').slice(0, -2)
-            : []
-        })
-      } else {
-        const err = new Error(output);
-        err.code = code;
-        reject(err);
-      }
-    }
+		function onExit(code) {
+			if (code === 0) {
+				resolve({
+					errors: [],
+				});
+			} else if (code === 3 || code === 4 /* validationError */) {
+				resolve({
+					errors: output.length
+						? output.split('\n').slice(0, -2)
+						: []
+				});
+			} else {
+				const err = new Error(output);
+				err.code = code;
+				reject(err);
+			}
+		}
 
-    worker.on('message', stdout);
-    worker.on('exit', onExit);
-    worker.on('error', err => {
-      console.error('Unexpected error event from worker: ' + err);
-      reject(err);
-    });
-  });
+		worker.on('message', stdout);
+		worker.on('exit', onExit);
+		worker.on('error', err => {
+			console.error('Unexpected error event from worker: ' + err);
+			reject(err);
+		});
+	});
 }
 
-module.exports.validateXML = validateXML
+module.exports.validateXML = validateXML;
--- worker.js
+++ worker.js
@@ -1,11 +1,11 @@
 const {workerData, parentPort} = require('worker_threads');
 
 function stdout(msg) {
-  parentPort.postMessage(msg);
+	parentPort.postMessage(msg);
 }
 
 require('./xmllint.js')({
-  xml: workerData.xml, schema: workerData.schema,
-  extension: '--schema',
-  stdout, stderr: stdout,
+	xml: workerData.xml, schema: workerData.schema,
+	extension: '--schema',
+	stdout, stderr: stdout,
 });
