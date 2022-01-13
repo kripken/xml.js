85ac18f45c479346cf99f260f70d2b5b5173933c
Florian Haftmann <florian.haftmann@informatik.tu-muenchen.de>
2022-01-13 11:09:38 +0100

    Tuned names.

--- src/pre.js
+++ src/pre.js
@@ -3,8 +3,8 @@ Module['preRun'] = function () {
 	const schemas = xmllintOptions['schemas'];
 	const xmls = xmllintOptions['xmls'];
 	const preloads = xmllintOptions['preloads'];
-	xmls.concat(schemas, preloads).forEach(function(xmlInfo) {
-		FS.createDataFile('/', xmlInfo['fileName'], intArrayFromString(xmlInfo['contents']), true, true);
+	xmls.concat(schemas, preloads).forEach(function(data) {
+		FS.createDataFile('/', data['fileName'], intArrayFromString(data['contents']), true, true);
 	});
 };
 
@@ -16,11 +16,11 @@ Module['arguments'] = ['--noout'];
 	const ext = `--${xmllintOptions['extension']}` // --schema or --relaxng
 	const schemas = xmllintOptions['schemas'];
 	const xmls = xmllintOptions['xmls'];
-	schemas.forEach(function(schemaInfo) {
+	schemas.forEach(function(schema) {
 		Module.arguments.push(ext);
-		Module.arguments.push(schemaInfo['fileName']);
+		Module.arguments.push(schema['fileName']);
 	});
-	xmls.forEach(function(xmlInfo) {
-		Module.arguments.push(xmlInfo['fileName']);
+	xmls.forEach(function(xml) {
+		Module.arguments.push(xml['fileName']);
 	});
 })();
