9281c503935ad2b46eda8d9d30c768885aa23c5c
Oskari Noppa <oskari@arpakuut.io>
2022-10-13 00:02:55 +0300

    Make tests use a multi-file schema
    This library supports schemas that are split to multiple files, via the
    preload-option. Make the specs verify that that feature keeps working.

--- test/test.js
+++ test/test.js
@@ -7,12 +7,20 @@ const xmlValidFormatted = fs.readFileSync('./test/test-valid-formatted.xml', 'ut
 const xmlInvalidFormatted = fs.readFileSync('./test/test-invalid-formatted.xml', 'utf8');
 const xmlValidC14n = fs.readFileSync('./test/test-valid-c14n.xml', 'utf8');
 const xmlInvalid = fs.readFileSync('./test/test-invalid.xml', 'utf8');
-const schema = fs.readFileSync('./test/test.xsd', 'utf8');
+const schemaOptions = {
+	schema: fs.readFileSync('./test/test.xsd', 'utf8'),
+	preload: [
+		{
+			fileName: 'comment.xsd',
+			contents: fs.readFileSync('./test/comment.xsd', 'utf8'),
+		}
+	]
+};
 
 async function testWithValidFile() {
 	const {rawOutput, normalized, ...result} = await xmllint.validateXML({
 		xml: {fileName: 'valid.xml', contents: xmlValid},
-		schema,
+		...schemaOptions,
 	});
 
 	assert.deepEqual(result, {valid: true, errors: []});
@@ -23,7 +31,8 @@ async function testWithValidFile() {
 
 async function testWithValidFileForFormat() {
 	const {valid, normalized} = await xmllint.validateXML({
-		xml: {fileName: 'valid.xml', contents: xmlValid}, schema,
+		xml: {fileName: 'valid.xml', contents: xmlValid},
+		...schemaOptions,
 		normalization: 'format'
 	});
 
@@ -43,7 +52,8 @@ async function testWithValidFileForFormatWithoutSchema() {
 
 async function testWithValidFileForC14n() {
 	const {valid, normalized} = await xmllint.validateXML({
-		xml: {fileName: 'valid.xml', contents: xmlValid}, schema,
+		xml: {fileName: 'valid.xml', contents: xmlValid},
+		...schemaOptions,
 		normalization: 'c14n'
 	});
 
@@ -53,7 +63,8 @@ async function testWithValidFileForC14n() {
 
 async function testWithInvalidFile() {
 	const {rawOutput, normalized, ...result} = await xmllint.validateXML({
-		xml: xmlInvalid, schema,
+		...schemaOptions,
+		xml: xmlInvalid,
 	});
 	const expectedErrorResult = {
 		valid: false,
@@ -88,7 +99,7 @@ async function testWithTwoFiles() {
 	];
 	const {rawOutput, normalized, ...result} = await xmllint.validateXML({
 		xml: input,
-		schema,
+		...schemaOptions,
 		normalization: 'format',
 	});
 	const expectedResultForBoth = {
